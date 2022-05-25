ef81b606fda836b0a08d32d172df6de7ef731e76
Will G <wgrout87@gmail.com>
2022-05-24 19:36:41 -0600

    api endpoints updated to join

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ const db = mysql.createConnection(
 
 // GET all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if (err) {
